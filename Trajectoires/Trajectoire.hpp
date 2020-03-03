5fa0dbbffeff34d5765e431f65bf201166bf4382
Xaltacas <adrien.piednoel@gmail.com>
2020-03-03 11:58:43 +0100

    test atom 2

---
+++ Trajectoires/Trajectoire.hpp
@@ -0,0 +1,15 @@
+#ifndef TRAJECTOIRE_HPP
+#define TRAJECTOIRE_HPP
+
+class Trajectoire{
+public:
+      virtual void nextPoint();
+
+private:
+      int i;
+      int xInit;
+      int yInit;
+
+};
+
+#endif
